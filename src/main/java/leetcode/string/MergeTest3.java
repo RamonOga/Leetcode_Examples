7dc8c50d23804c614c5e6ae4f0c3b77796753738
Roman Markelov <Roman.sercent@gmail.com>
2021-06-24 13:58:05 +0300

    merge test commit #3

--- src/main/java/leetcode/string/MergeTest3.java
+++ src/main/java/leetcode/string/MergeTest3.java
@@ -2,6 +2,8 @@ package leetcode.string;
 
 public class MergeTest3 {
     public static void main(String[] args) {
-
+        MergeTest mt = new MergeTest(11, "nameMT");
+        MergeTest2.method(mt);
+        System.out.println(mt.getAge() + " : " + mt.getName());
     }
 }
