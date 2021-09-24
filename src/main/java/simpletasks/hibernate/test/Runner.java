c0493b6cf4789f4bf873c0f947467d0c8980af3f
Ramon Markelov <Roman.sercent@gmail.com>
2021-09-24 13:47:07 +0300

    one branch test 1

---
+++ src/main/java/simpletasks/hibernate/test/Runner.java
@@ -0,0 +1,21 @@
+package simpletasks.hibernate.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Runner {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            System.out.println("!!!");
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
